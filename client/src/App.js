17b0cdaecfd6b8719cfb86869dda2d4f107d0490
thesergioliveira <thesergioliveira@gmail.com>
2021-10-11 08:03:16 +0200

    adding latest code changes

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,70 @@
-// import {BrowserRouter as Router, Route} from "react-router-dom";
+import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
+import Home from './components/Home';
+import About from './components/About';
+import Blog from './components/Blog';
+import Products from './components/Products';
 
 function App() {
+  const stylingObject = {
+    ul: {
+      width: '100vw',
+      margin: 'auto 0',
+      display: 'flex',
+      flexDirection: 'row',
+      fontSize: '2rem',
+      gap: '1rem',
+    },
+    li: {
+      listStyle: 'none',
+    },
+    a: {
+      textDecoration: 'none',
+      color: 'gray',
+    },
+    text: {
+      textAlign: 'center',
+    },
+  };
   return (
-    <div>
-     <h1 style={{"textAlign":"center"}}>Authentication App</h1> 
-    </div>
+    <Router>
+      <header>
+        <div className="whatever">
+          <h1 style={{ textAlign: 'center' }}>Authentication App</h1>
+          <nav>
+            <ul style={stylingObject.ul}>
+              <li style={stylingObject.li}>
+                <Link style={stylingObject.a} to={'/'}>
+                  Home
+                </Link>
+              </li>
+              <li style={stylingObject.li}>
+                <Link style={stylingObject.a} to={'/about'}>
+                  About
+                </Link>
+              </li>
+              <li style={stylingObject.li}>
+                <Link style={stylingObject.a} to={'/blog'}>
+                  Blog
+                </Link>
+              </li>
+              <li style={stylingObject.li}>
+                <Link style={stylingObject.a} to={'/products'}>
+                  Products
+                </Link>
+              </li>
+            </ul>
+          </nav>
+        </div>
+      </header>
+      <main>
+        <Switch>
+          <Route path="/" exact render={Home}></Route>
+          <Route path="/about" exact render={About}></Route>
+          <Route path="/blog" exact render={Blog}></Route>
+          <Route path="/products" exact render={Products}></Route>
+        </Switch>
+      </main>
+    </Router>
   );
 }
 
